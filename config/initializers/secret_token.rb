248ab9f43888139df734d340b732390b316239b2
NightingaleGit <messagetruck@gmail.com>
2015-01-16 06:58:53 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6a87ea970f14860aaf1baa0b87a04c15f349d53c6256aa2a6987b2068e8495a8d07b222ab126cd56897b59370df56badf932b9ea50c9a63fe7ac57a1423b89d9'
